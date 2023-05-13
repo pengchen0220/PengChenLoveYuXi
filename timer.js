23dfb0961328c0dcefcbf3774dc26cf87a9a6f2c
Chen Peng <chen.peng@seamoney.com>
2023-05-13 13:21:16 +0800

    add webpage

---
+++ timer.js
@@ -0,0 +1,25 @@
+var startTime = new Date("February 2, 2023 00:00:00").getTime();
+var currentTime;
+
+function updateTime() {
+  currentTime = Date.now();
+  var elapsedTime = currentTime - startTime;
+  var totalSeconds = Math.floor(elapsedTime / 1000);
+  var days = Math.floor(totalSeconds / 86400);
+  var hours = Math.floor((totalSeconds - (days * 86400)) / 3600);
+  var minutes = Math.floor((totalSeconds - (days * 86400) - (hours * 3600)) / 60);
+  var seconds = totalSeconds - (days * 86400) - (hours * 3600) - (minutes * 60);
+  var formattedTime = pad(days) + "-" + pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+  var result = "|   " + totalSeconds + "   | Peng Chen |   Yu Xi   |"
+  document.getElementById("timer").innerHTML = result;
+}
+
+function pad(num, size) {
+  var numString = num.toString();
+  while (numString.length < size) {
+    numString = "0" + numString;
+  }
+  return numString;
+}
+
+setInterval(updateTime, 1000);
